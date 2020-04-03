82eccf9ef6176017685be7f6e318c2d3b50f49e4
Michelle Morin <morin2m@gmail.com>
2020-04-03 16:46:26 -0700

    finalize migrations

---
+++ ParksClient/Migrations/20200403233916_Finalize.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksClient.Migrations
+{
+    public partial class Finalize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
